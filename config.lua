a71fef6470bac2184ea265956813e5424cbd0c48
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-07-13 12:09:56 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 Config = {}
 
 Config.Framework = 'ESX' -- ESX or QBCore
+Config.Debug = false
+Config.SpamCooldown = 2000 -- 2000 = 2 Seconds
 
 Config.MaxCartridges = 1  -- How many Shoots per Cartridge
 Config.ReloadKey = 45  -- (https://docs.fivem.net/docs/game-references/controls/)
